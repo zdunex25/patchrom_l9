7951507cbfea4d582c307ec7b8a579076a4f1e79
zdunex25 <zdunex25@gmail.com>
2014-01-03 11:47:52 +0100

    update of miui v5

--- customize_framework.sh
+++ customize_framework.sh
@@ -54,9 +54,9 @@ then
     rm -rf $BUILD_OUT/$SEP_FRAME
 fi
 
-if [ $2 = "$BUILD_OUT/android.policy" ]
-then
+#if [ $2 = "$BUILD_OUT/android.policy" ]
+#then
     #fix wrong ids on lockscreen
     #sed -i -e 's/60c0283/60c028f/' $BUILD_OUT/android.policy/smali/com/android/internal/policy/impl/MiuiSimPINUnlockScreen.smali
     #sed -i -e 's/60c0286/60c0279/' $BUILD_OUT/android.policy/smali/com/android/internal/policy/impl/MiuiSimPINUnlockScreen.smali
-fi
+#fi
